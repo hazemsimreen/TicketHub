f13d270c808d5cefc11a884910178e9ce5d3d82d
Baraa Shihab <braashhab399@gmail.com>
2026-08-06 16:54:21 +0300

    - Added realtime notification abstraction:   - IRealtimeNotifier interface   - Stub implementation for future realtime integration.

---
+++ BusinessLogic/Services/NullRealtimeNotifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using BusinessLogic.Abstractions;
+
+namespace BusinessLogic.Services;
+
+public class NullRealtimeNotifier : IRealtimeNotifier
+{
+    public Task NotifyAsync(
+        string userId,
+        string message)
+    {
+        return Task.CompletedTask;
+    }
+}
